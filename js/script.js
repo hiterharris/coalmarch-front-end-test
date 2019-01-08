c471862709fd21cf390be2a0ccbed0e9b79a3385
hiterharris <hiterharris@comcast.net>
2019-01-08 15:08:21 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+[if lte IE 8]>
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
+<![endif]
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
+<script>
+ hbspt.forms.create({
+               portalId: "4488058",
+               formId: "69bb4b64-bbc6-420f-a63a-58f3fe159a27"
+});
+</script>
