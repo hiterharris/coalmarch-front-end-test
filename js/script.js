1c3b788900e8df0a097209c71077cbfa11fda34a
hiterharris <hiterharris@comcast.net>
2019-01-09 13:44:01 -0500

    menu hover, modal, button jquery events

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
-[if lte IE 8]>
-<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
-<![endif]
-<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
-<script>
- hbspt.forms.create({
-               portalId: "4488058",
-               formId: "69bb4b64-bbc6-420f-a63a-58f3fe159a27"
+$(document).ready( () => {
+    $(".contact-modal").hide();
+    $(".contact-button").on( 'click', () => {
+        $(".contact-modal").show();
+    });
+    $(".close").on( 'click', () => {
+        $(".contact-modal").hide();
+    });
 });
-</script>
+
+$(document).ready( () => {
+    $(".cta-button").on( 'click', () => {
+        $(".contact-modal").show();
+    });
+    $(".close").on( 'click', () => {
+        $(".contact-modal").hide();
+    });
+});
